ec60e41c5595074a0d83d734cbd49b9eed38a7ff
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-08-11 20:23:06 -0600

    Add custom color to system bars

--- app/src/main/java/com/rodrigocarreon/spideytracker/MainActivity.kt
+++ app/src/main/java/com/rodrigocarreon/spideytracker/MainActivity.kt
@@ -2,25 +2,32 @@ package com.rodrigocarreon.spideytracker
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.rodrigocarreon.spideytracker.ui.screens.MapScreen
 import com.rodrigocarreon.spideytracker.ui.theme.SpideyTrackerTheme
+import android.graphics.Color as AndroidColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(AndroidColor.BLACK),
+            navigationBarStyle = SystemBarStyle.dark(AndroidColor.BLACK)
+        )
+
         setContent {
             SpideyTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = Color.Black
+                ) { innerPadding ->
                     MapScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
