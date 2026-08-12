ddfa16ca1d45a90fea786ea6a89fd2eea155340c
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-08-11 20:05:03 -0600

    Add decorative frame

--- app/src/main/java/com/rodrigocarreon/spideytracker/ui/screens/MapScreen.kt
+++ app/src/main/java/com/rodrigocarreon/spideytracker/ui/screens/MapScreen.kt
@@ -1,12 +1,22 @@
 package com.rodrigocarreon.spideytracker.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -31,6 +41,12 @@ fun MapScreen (
     val icon_confirmed = R.drawable.green_pin
     val icon_event = R.drawable.white_pin
 
+    val spideyBanner = R.drawable.banner
+    val topFrame = R.drawable.topframe
+    val bottomFrame = R.drawable.bottomframe
+    val rightFrame = R.drawable.rightframe
+    val leftFrame = R.drawable.leftframe
+
     val sightings by viewModel.sightings.collectAsState()
     LaunchedEffect(Unit) {
         viewModel.getSightings()
@@ -41,26 +57,62 @@ fun MapScreen (
         position = CameraPosition.fromLatLngZoom(newYork, 11f)
     }
 
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    ) {
-        sightings.forEach { sighting ->
-            val iconResId = when(sighting.type){
-                SightingType.RUMOR -> icon_rumor
-                SightingType.CONFIRMED -> icon_confirmed
-                SightingType.EVENT -> icon_event
-                SightingType.UNKWOWN -> icon_event
+    Box(modifier = modifier.fillMaxSize()){
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            sightings.forEach { sighting ->
+                val iconResId = when(sighting.type){
+                    SightingType.RUMOR -> icon_rumor
+                    SightingType.CONFIRMED -> icon_confirmed
+                    SightingType.EVENT -> icon_event
+                    SightingType.UNKWOWN -> icon_event
+                }
+
+                val mapIcon = bitmapDescriptorFromVector(context, iconResId)
+
+                Marker(
+                    state = MarkerState(position = LatLng(sighting.latitude, sighting.longitude)),
+                    title = sighting.title,
+                    snippet = sighting.description,
+                    icon = mapIcon
+                )
             }
-
-            val mapIcon = bitmapDescriptorFromVector(context, iconResId)
-
-            Marker(
-                state = MarkerState(position = LatLng(sighting.latitude, sighting.longitude)),
-                title = sighting.title,
-                snippet = sighting.description,
-                icon = mapIcon
-            )
         }
+
+        Image(
+            painter = painterResource(id = topFrame),
+            null,
+            modifier = Modifier.align(Alignment.TopCenter).fillMaxWidth().size(30.dp),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Image(
+            painter = painterResource(id = bottomFrame),
+            null,
+            modifier = Modifier.align(Alignment.BottomCenter).fillMaxWidth().size(30.dp),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Image(
+            painter = painterResource(id = leftFrame),
+            null,
+            modifier = Modifier.align(Alignment.CenterStart).fillMaxHeight().size(30.dp),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Image(
+            painter = painterResource(id = rightFrame),
+            null,
+            modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight().size(30.dp),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Image(
+            painter = painterResource(id = spideyBanner),
+            contentDescription= "Logo SpideyTracker",
+            modifier = Modifier.align(Alignment.TopCenter).padding(top = 12.dp).fillMaxWidth(0.75f).size(40.dp)
+        )
     }
 }
